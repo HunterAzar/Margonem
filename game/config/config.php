efbb44cbfae5f4544959d329f7a50db2e99c8452
Tobiasz <0tobik09@gmail.com>
2021-03-12 16:52:42 +0100

    Update to php7+

--- game/config/config.php
+++ game/config/config.php
@@ -4,12 +4,19 @@ $player = 'root';
 $pass = '';
 $db = 'margonem';
 
-$connect = @mysql_connect($host, $player, $pass) or die('socket error - no connect');
-@mysql_select_db($db, $connect) or die('socket error - no db');
-@mysql_query("SET NAMES 'utf8-polish-ci'");
+$connect = @mysqli_connect($host, $player, $pass) or die('socket error - no connect');
+if(is_string($connect)){
+    echo("Błąd łączenia z DB");
+    echo(mysqli_errno($connect));
+}
+@mysqli_select_db($connect,$db) or die('socket error - no db');
+@mysqli_query($connect,"SET NAMES 'utf8-polish-ci'");
 
 session_start();
+if(isset($_SESSION['player'])){
+    $player = @mysqli_fetch_array(@mysqli_query($connect,"select * from player where id = '".$_SESSION['player']."' limit 1"));
+    printf($player['mapa']);
+    $mapa = @mysqli_fetch_array(@mysqli_query($connect,"select * from mapa where id = ".$player['mapa']." limit 1"));
+}
 
-$player = @mysql_fetch_array(@mysql_query("select * from player where id = '".$_SESSION['player']."' limit 1"));
-$mapa = @mysql_fetch_array(@mysql_query("select * from mapa where id = ".$player['mapa']." limit 1"));
 ?>
--- logowanie.php
+++ logowanie.php
@@ -1,12 +1,12 @@
 <?php require_once('game/config/config.php'); ?>
 <?php
 
-$db_player = @mysql_num_rows(@mysql_query("select * from player"));
-$code = @mysql_query("select * from player order by id asc");
+$db_player = @mysqli_num_rows(@mysqli_query($connect,"select * from player"));
+$code = @mysqli_query($connect,"select * from player order by id asc");
 if(!empty($_POST['login'])){
 if(!empty($_POST['haslo'])){ 
 if(!empty($_POST['player'])){
-$db_player_dane = @mysql_fetch_array(@mysql_query("select * from player where id = '".$_POST['player']."' and login = '".$_POST['login']."' and haslo = '".$_POST['haslo']."' limit 1"));
+$db_player_dane = @mysqli_fetch_array(@mysqli_query($connect,"select * from player where id = '".$_POST['player']."' and login = '".$_POST['login']."' and haslo = '".$_POST['haslo']."' limit 1"));
 if(!empty($db_player_dane)){
 $_SESSION['player'] = $_POST['player'];
 header('Location: game/game.php');
@@ -26,7 +26,7 @@ echo "<span style='color: red; font-weight: bold;'>Bledne Haslo</span><hr>";
 <form action='logowanie.php' method='post'>
 <select name="player">
 <?php
-while($wczytuje = @mysql_fetch_array($code)){
+while($wczytuje = @mysqli_fetch_array($code)){
 echo "<option value='".$wczytuje['id']."'>".$wczytuje['nazwa']."(".$wczytuje['poziom'].")(".$wczytuje['profesja'].")</option>";
 }
 ?>
